0424bd83c399c9b286fabf1ae31e9d4da2b2e699
Kevin Chung <chungkev7@gmail.com>
2020-01-23 13:22:52 -0500

    Added quantity increase/decrease functionality

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -11,6 +11,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +20,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity += 1;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        if (quantity != 0) {
+            quantity -= 1;
+        }
+        display(quantity);
+    }
+    /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2 * 5);
+        displayPrice(quantity * 5);
     }
 
     /**
