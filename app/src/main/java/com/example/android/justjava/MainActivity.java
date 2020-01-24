c531cb880a450e0f8bf5c24f2e97b4d54f671e1a
Kevin Chung <chungkev7@gmail.com>
2020-01-23 23:49:42 -0500

    Added additional message on submit and changed submit button background color

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -36,11 +36,14 @@ public class MainActivity extends AppCompatActivity {
         }
         display(quantity);
     }
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * 5);
+        int price = quantity * 5;
+        String priceMessage="Total: $" + price + "\n\nThank you!";
+        displayMessage(priceMessage);
     }
 
     /**
@@ -59,4 +62,11 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
+    /**
+     * This method displays the given text on the screen
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
 }
