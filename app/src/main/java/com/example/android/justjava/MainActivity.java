912a49cc9db5abf0c623b48c538acd2d72d887bd
Kevin Chung <chungkev7@gmail.com>
2020-01-28 12:18:25 -0500

    Added Spanish translations for texts and styling

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -50,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        EditText name = (EditText) findViewById(R.id.name_field);
-        String userName = name.getText().toString();
+        EditText userName = (EditText) findViewById(R.id.name_field);
+        String name = userName.getText().toString();
 
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
@@ -59,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         boolean hasChocolate = chocolateCheckBox.isChecked();
 
         int price = calculatePrice(hasWhippedCream, hasChocolate);
-        String priceMessage = createOrderSummary(userName, price, hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
 
         displayMessage(priceMessage);
 
@@ -97,19 +99,19 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Create a summary of the order
      *
-     * @param userName is the name the user entered
+     * @param name is the name the user entered
      * @param hasWhippedCream is whether or not the user wants whipped cream topping
      * @param hasChocolate is whether or not the user wants chocolate topping
      * @param price of the order
      * @return text summary
      */
-    private String createOrderSummary(String userName, int price, boolean hasWhippedCream, boolean hasChocolate) {
-        String priceMessage = "Name: " + userName;
-        priceMessage += "\n\nWhipped cream added? " + hasWhippedCream;
-        priceMessage += "\n\nChocolate added? " + hasChocolate;
-        priceMessage += "\n\nQuantity: " + quantity;
-        priceMessage += "\n\nTotal: $" + price;
-        priceMessage += "\n\nThank you!";
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate) {
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n\n" + getString(R.string.order_summary_whipped_cream, hasWhippedCream);
+        priceMessage += "\n\n" + getString(R.string.order_summary_chocolate, hasChocolate);
+        priceMessage += "\n\n" + getString(R.string.order_summary_quantity, quantity);
+        priceMessage += "\n\n" + getString(R.string.order_summary_price, NumberFormat.getCurrencyInstance().format(price));
+        priceMessage += "\n\n" + getString(R.string.thank_you);
 
         return priceMessage;
     }
